dae01668104dc110d34da7556621dd737218af75
karl.nordstrom@gmail.com <karl.nordstrom@gmail.com@16d75961-40e5-f889-0403-368a60771a35>
2011-03-15 15:28:51 +0000

    bugfixes - changed the variablename dataset to dataset_shoremapmle... as this is used as global variable...

--- trunk/SHOREmap_confInt.R
+++ trunk/SHOREmap_confInt.R
@@ -1,33 +1,37 @@
 #chromosome,positions, background_count,foreground_count and error_count are vectors of the same length
-ShoreMap.confint <- function(chromosome,positions,background_count,foreground_count, error_count,foreground_frequency=1,level=0.99,minWindow=10){
- data<- cbind(chromosome,positions,foreground_count,background_count,error_count)
- data<- data[rowSums(data[,3:5])>0,]
+ShoreMap.confint <- function(chromosome,positions,background_count,foreground_count, error_count,foreground_frequency=1,level=0.99,minWindow=10,recurse=TRUE){
+ internalData<- cbind(chromosome,positions,foreground_count,background_count,error_count)
+ internalData<- internalData[rowSums(internalData[,3:5])>0,]
  #perhaps not the nicest solution
- assign("dataset",data,".GlobalEnv")
- res<- identify_peaks(0,length(data[,2]),foreground_frequency,level,minWindow)
- apply(res,1,function(x) t(c(start=data[x[1],2], stop=data[x[1]+x[2]-1,2],p.value=-1*(x[3]+x[2]))))
+ assign("dataset_shoremapmle",internalData,".GlobalEnv")
+ res<- identify_peaks(0,length(internalData[,2]),foreground_frequency,level,minWindow,recurse)
+ apply(res,1,function(x) t(c(start=internalData[x[1],2], stop=internalData[x[1]+x[2]-1,2],p.value=-1*(x[3]+x[2]))))
 }
 
-identify_peaks <- function(indexL,indexH,frequency,level,minWindow){
+identify_peaks <- function(indexL,indexH,frequency,level,minWindow,recurse){
  beststart=floor(optimize(maxConfPos,lower=indexL,upper=indexH,freq=frequency,neighbors=floor(minWindow/4),size=minWindow,minWindow=minWindow)$minimum[1])
 # might avoid a check by checking the first region
 # maxConf(c(beststart,10),level=level,freq=frequency,indexL=indexL,indexH=indexH,minWindow=minWindow)
  res<-extend(beststart,minWindow,level,frequency,indexL,indexH,minWindow)
  if(res[1,3]<0){
   #try to find other peaks to the left and right
-  resL<- identify_peaks(indexL,max(res[1,1],indexL,0),frequency,level,minWindow)
-  resH<- identify_peaks(min(res[1,1]+res[1,2],indexH,length(dataset[,2])),indexH,frequency,level,minWindow)
-  if(resL[1,3]<0){
-   if(resH[1,3]<0){
-    #both good
-    rbind(resL,res,resH)
+  if(recurse){
+   resL<- identify_peaks(indexL,max(res[1,1],indexL,0),frequency,level,minWindow)
+   resH<- identify_peaks(min(res[1,1]+res[1,2],indexH,length(dataset_shoremapmle[,2])),indexH,frequency,level,minWindow)
+   if(resL[1,3]<0){
+    if(resH[1,3]<0){
+     #both good
+     rbind(resL,res,resH)
+    }else{
+     #low good
+     rbind(resL,res)
+    }
+   }else if(resH[1,3]<0){
+    #high good
+    rbind(res,resH)
    }else{
-    #low good
-    rbind(resL,res)
+    res
    }
-  }else if(resH[1,3]<0){
-   #high good
-   rbind(res,resH)
   }else{
    res
   }
@@ -45,7 +49,7 @@ loglikelihood_mult <- function(P1=0.5,err=0.01,index=0,size=0){
   pe<- 2*err/3
   p2<- 1-p1-pe
   p.all <- c(p1,p2,pe)
-  -sum(apply(dataset[index:(index+size-1),],1,function(x){dmultinom(x=c(x[3],x[4],x[5]),prob=p.all,log=TRUE)}))
+  -sum(apply(dataset_shoremapmle[index:(index+size-1),],1,function(x){dmultinom(x=c(x[3],x[4],x[5]),prob=p.all,log=TRUE)}))
  }
 }
 
@@ -59,7 +63,7 @@ maxConf<-function(x,level=0.95,freq=0,indexL=0,indexH=Inf,minWindow=10){
  start<-floor(x[1])
  size<-floor(x[2])
  indexL<- max(0,indexL)
- indexH<- min(length(dataset[,2]),indexH)
+ indexH<- min(length(dataset_shoremapmle[,2]),indexH)
  if(size<minWindow){
   110000-size+minWindow
  }else if(start<indexL){
@@ -78,6 +82,7 @@ maxConf<-function(x,level=0.95,freq=0,indexL=0,indexH=Inf,minWindow=10){
     res<- size+p
    }
   }
+#  print(paste(start,size,res,sep="   "))
   res
  }
 }
@@ -87,7 +92,7 @@ maxConfPos<-function(x,level=0.95,freq=0,size=10,neighbors=2,indexL=0,indexH=Inf
  #function to optimize in order to find an initial start point
  start<-floor(x[1])
  indexL<- max(0,indexL)
- indexH<- min(length(dataset[,2]),indexH)
+ indexH<- min(length(dataset_shoremapmle[,2]),indexH)
  if(start<indexL+neighbors){
   110000-indexL-neighbors+start
  }else if(size<minWindow){
@@ -107,7 +112,7 @@ extend <- function(beststart,bestsize=10,level=0.95,freq=1,indexL=0,indexH=Inf,m
  #given a window it extends this as far as possible to the left and right without exceeding the confidence level
  bestvalue=Inf
  indexL<- max(0,indexL)
- indexH<- min(length(dataset[,2]),indexH)
+ indexH<- min(length(dataset_shoremapmle[,2]),indexH)
  #a first optimization
  nextTest<- optim(fn=maxConf,par=c(beststart,bestsize),control=list(ndeps=c(1,1)),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
 
@@ -157,7 +162,7 @@ extend <- function(beststart,bestsize=10,level=0.95,freq=1,indexL=0,indexH=Inf,m
    bestsize<-bestsize+i
   }
   #optimization again before reitteration
-  nextTest<- optim(fn=maxConf,par=c(beststart,bestsize),control=list(ndeps=c(1,1)),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+  nextTest<- optim(fn=maxConf,par=c(beststart,bestsize),control=list(ndeps=c(1,1),maxit=100),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
  }
  t(as.matrix(c(beststart,bestsize,bestvalue)))
 }
