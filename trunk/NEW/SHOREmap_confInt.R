7b8b9a0e5b16fb275451154b9c7c891affc4dd82
karl.nordstrom@gmail.com <karl.nordstrom@gmail.com@16d75961-40e5-f889-0403-368a60771a35>
2011-03-29 08:41:47 +0000

    Small changes and speed improvements

--- trunk/NEW/SHOREmap_confInt.R
+++ trunk/NEW/SHOREmap_confInt.R
@@ -3,29 +3,40 @@
 #616 not a valid peak
 #617 the examined window was too small compared to the internal windowsize (minWindow)
 
-#chromosome,positions, background_count,foreground_count and error_count are vectors of the same length
-ShoreMap.confint <- function(chromosome,positions,background_count,foreground_count, error_count,foreground_frequency=1,level=0.99,recurse=TRUE){
- internalData<- cbind(chromosome,positions,foreground_count,background_count,error_count)
+#chromosome,positions, background_count,forground_count and error_count are vectors of the same length
+ShoreMap.confint <- function(chromosome,positions,background_count,forground_count, error_count,forground_frequency=1,level=0.99,recurse=TRUE){
+ internalData<- cbind(chromosome,positions,forground_count,background_count,error_count)
  internalData<- internalData[rowSums(internalData[,3:5])>0,]
  #perhaps not the nicest solution
  assign("dataset_shoremapmle",internalData,".GlobalEnv")
+ assign("storage_shoremapmle",matrix(c(-1,-1,-1),nrow=1),".GlobalEnv")
+# assign("savedCalc_shoremapmle",0,".GlobalEnv")
  minWindow=max(10,floor(length(internalData[,2])/1000))
- res<- identify_peaks(1,length(internalData[,2]),foreground_frequency,level,minWindow,recurse)
- #rm(dataset_shoremapmle)
+ freqs<-apply(dataset_shoremapmle,1,function(x) x[3]/sum(x[3:5]))
+ ps_global<-sapply(1:(length(dataset_shoremapmle[,1])-minWindow+1),function(x) {cur_freqs<-freqs[x:(x+minWindow-1)]; p<-tryCatch(t.test(cur_freqs,mu=forground_frequency)$p.value,error=function(err) -616);ifelse(is.na(p),-616,p)})
+  bestsize<-minWindow
+  while(sum(ps_global<0)>0){
+   bestsize<-bestsize+5 #OK to increase windowsize by 5?
+   print(bestsize)
+   ps_global<-sapply(1:(length(dataset_shoremapmle[,1])-bestSize+1),function(x) {cur_freqs<-freqs[x:(x+bestsize-1)]; p<-tryCatch(t.test(cur_freqs,mu=forground_frequency)$p.value,error=function(err) -616);ifelse(is.na(p),-616,p)})
+  }
+ res<- identify_peaks(1,length(internalData[,2]),forground_frequency,level,minWindow,ps_global,bestsize,recurse)
+ rm(dataset_shoremapmle)
+ rm(storage_shoremapmle)
  apply(res,1,function(x) t(c(start=ifelse(x[3]<0,internalData[x[1],2],0), stop=ifelse(x[3]<0,internalData[x[1]+x[2]-1,2],0),p.value=ifelse(x[3]<0,-1*(x[3]+x[2]),x[3]))))
 }
 
-identify_peaks <- function(indexL,indexH,frequency,level,minWindow,recurse){
+identify_peaks <- function(indexL,indexH,frequency,level,minWindow,ps_global,bestsize,recurse){
  if(indexH-indexL>minWindow){
-  freqs<-apply(dataset_shoremapmle,1,function(x) x[3]/sum(x[3:5]))
-  cur_indices<-indexL:(indexH-minWindow+1)
-  ps<-sapply(cur_indices,function(x) {cur_freqs<-freqs[x:(x+minWindow-1)];p<-616; tryCatch(p<-t.test(cur_freqs,mu=frequency)$p.value,error=function(err) {p<-ifelse(mean(cur_freqs)==frequency,1,0)});ifelse(is.na(p)&&mean(cur_freqs)==0,1,p)})
+  
+  cur_indices<-indexL:(indexH-bestsize+1)
+  ps<-ps_global[cur_indices]
   if(max(ps)>0.001){
    #try to find peaks
    starts<- cur_indices[ps==max(ps)]
    while(length(starts)>0){
-    beststart<- starts[1]
-    res<-extend(beststart,minWindow,level,frequency,indexL,indexH,minWindow)
+    beststart<- starts[ceiling(length(starts)/2)]
+    res<-extend(beststart,bestsize,level,frequency,indexL,indexH,minWindow)
     if(res[1,3]>0){
      if(length(starts)==1){
       return(t(as.matrix(c(1,1,616))))
@@ -35,8 +46,8 @@ identify_peaks <- function(indexL,indexH,frequency,level,minWindow,recurse){
      }
     }else if(recurse){
      if(res[1,3]<0){
-      resL<- identify_peaks(indexL,res[1,1],frequency,level,minWindow,recurse)
-      resH<- identify_peaks(res[1,1]+res[1,2],indexH,frequency,level,minWindow,recurse)
+      resL<- identify_peaks(indexL, res[1,1], frequency, level, minWindow, ps_global, bestsize, recurse)
+      resH<- identify_peaks(res[1,1]+res[1,2], indexH, frequency, level, minWindow, ps_global, bestsize, recurse)
       if(resL[1,3]<0){
        if(resH[1,3]<0){
         #both good
@@ -108,19 +119,29 @@ maxConf<-function(x,level=0.95,freq=0,indexL=0,indexH=Inf,minWindow=10){
  }else if(start+size-1>indexH){
   110000+indexH+1-start-size
  }else{
-  fit<-multll(start,size)
-  if(fit@min>100000){
-   res<-fit@min
+  #check storage
+  if(sum(storage_shoremapmle[,1]==start&storage_shoremapmle[,2]==size)==1){
+   res<-storage_shoremapmle[storage_shoremapmle[,1]==start&storage_shoremapmle[,2]==size,3]
+#   print(paste(start,size,res,sep=" # "))
+#   assign("savedCalc_shoremapmle",savedCalc_shoremapmle+1,".GlobalEnv")
+   res
   }else{
-   p<- pchisq(-2*(loglikelihood_mult(fit@coef[1],fit@coef[2],start,size)-loglikelihood_mult(freq,fit@coef[2],start,size)),1)
-   if(p<=level){
-    res<- -size-p
+   #if not in storage, calculate
+   fit<-multll(start,size)
+   if(fit@min>100000){
+    res<-fit@min
    }else{
-    res<- size+p
+    p<- pchisq(-2*(loglikelihood_mult(fit@coef[1],fit@coef[2],start,size)-loglikelihood_mult(freq,fit@coef[2],start,size)),1)
+    if(p<=level){
+     res<- -size-p
+    }else{
+     res<- size+p
+    }
    }
+#   print(paste(start,size,res,sep="   "))
+   assign("storage_shoremapmle",rbind(storage_shoremapmle,c(start,size,res)),".GlobalEnv")
+   res
   }
-#  print(paste(start,size,res,sep="   "))
-  res
  }
 }
 
@@ -151,7 +172,7 @@ extend <- function(beststart,bestsize=10,level=0.95,freq=1,indexL=0,indexH=Inf,m
  indexL<- max(1,indexL)
  indexH<- min(length(dataset_shoremapmle[,2]),indexH)
  #a first optimization
- nextTest<- optim(fn=maxConf,par=c(beststart,bestsize),control=list(ndeps=c(1,1)),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+ nextTest<- optim(fn=maxConf,par=c(beststart,bestsize),control=list(ndeps=c(1,1)),level=level,freq=freq,indexL=max(indexL,beststart-10*minWindow),indexH=min(indexH,beststart+bestsize+10*minWindow),minWindow=minWindow)
 
 
 
@@ -159,10 +180,50 @@ extend <- function(beststart,bestsize=10,level=0.95,freq=1,indexL=0,indexH=Inf,m
   bestvalue<-nextTest$value
   beststart<-floor(nextTest$par[1])
   bestsize<-floor(nextTest$par[2])
+  #alternatively right-left extension by minWindow
   i<-1
   lastvalue<-bestvalue
-  curvalue<-maxConf(c(beststart-floor(i/2),bestsize+i),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+  curvalue<-maxConf(c(beststart-floor(i/2)*minWindow,bestsize+i*minWindow),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow) 
+  while(i<1000&&curvalue<lastvalue){
+   lastvalue<-curvalue
+   i<-i+1
+   curvalue<-maxConf(c(beststart-floor(i/2)*minWindow,bestsize+i*minWindow),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+  }
+  i<-i-1
+  beststart<-beststart-floor(i/2)*minWindow
+  bestsize<-bestsize+i*minWindow
+  if(i%%2==0){
+   #if the extension breaks down on the right, continue on to expand left
+   i<-1
+   lastvalue<-maxConf(c(beststart,bestsize),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+   curvalue<-maxConf(c(beststart-i*minWindow,bestsize+i*minWindow),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+   while(i<1000&&curvalue<lastvalue){
+    lastvalue<-curvalue
+    i<-i+1
+    curvalue<-maxConf(c(beststart-i*minWindow,bestsize+i*minWindow),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+   }
+   i<-i-1
+   beststart<-beststart-i*minWindow
+   bestsize<-bestsize+i*minWindow
+  }else{
+   #if the extension breaks down on the left, continue on to expand right
+   i<-1
+   lastvalue<-maxConf(c(beststart,bestsize),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+   curvalue<-maxConf(c(beststart,bestsize+i*minWindow),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+   while(i<1000&&curvalue<lastvalue){
+    lastvalue<-curvalue
+    i<-i+1
+    curvalue<-maxConf(c(beststart,bestsize+i*minWindow),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+   }
+   i<-i-1
+   beststart<-beststart
+   bestsize<-bestsize+i*minWindow
+  }
   #alternatively right-left extension
+  i<-1
+  lastvalue<- maxConf(c(beststart,bestsize),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+  curvalue<-maxConf(c(beststart-floor(i/2),bestsize+i),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
+  
   while(i<1000&&curvalue<lastvalue){
    lastvalue<-curvalue
    i<-i+1
@@ -174,7 +235,7 @@ extend <- function(beststart,bestsize=10,level=0.95,freq=1,indexL=0,indexH=Inf,m
   if(i%%2==0){
    #if the extension breaks down on the right, continue on to expand left
    i<-1
-   lastvalue<-bestvalue
+   lastvalue<-maxConf(c(beststart,bestsize),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
    curvalue<-maxConf(c(beststart-i,bestsize+i),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
    while(i<1000&&curvalue<lastvalue){
     lastvalue<-curvalue
@@ -187,7 +248,7 @@ extend <- function(beststart,bestsize=10,level=0.95,freq=1,indexL=0,indexH=Inf,m
   }else{
    #if the extension breaks down on the left, continue on to expand right
    i<-1
-   lastvalue<-bestvalue
+   lastvalue<-maxConf(c(beststart,bestsize),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
    curvalue<-maxConf(c(beststart,bestsize+i),level=level,freq=freq,indexL=indexL,indexH=indexH,minWindow=minWindow)
    while(i<1000&&curvalue<lastvalue){
     lastvalue<-curvalue
