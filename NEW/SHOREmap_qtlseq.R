4db296621116f9759135fd6c6a823cb26fe07c9c
karl.nordstrom <karl.nordstrom@gmail.com>
2011-08-11 11:53:15 +0000

    Before automization

--- NEW/SHOREmap_qtlseq.R
+++ NEW/SHOREmap_qtlseq.R
@@ -1,6 +1,13 @@
+args<-commandArgs(trailingOnly=TRUE)
+
+high_file<-args[1]
+low_file<-args[2]
+qtl_file<-args[3]
+
 #prep data
 hs<-read.table("high.seq")
 ls<-read.table("low.seq")
+
 ##merge
 hs.freq<- apply(hs,1,function(x) x[3]/sum(x[3:5]))
 ls.freq<- apply(ls,1,function(x) x[3]/sum(x[3:5]))
@@ -9,14 +16,128 @@ data<- cbind(hs[,1:2],hs.freq,hs[,3:5],ls.freq,ls[,3:5])
 p<-apply(data,1,function(x) fisher.test(matrix(as.numeric(x[c(4,5,8,9)]),nrow=2))$p.value)
 lp<- -log(p)
 
-qtls<-data.frame(index=c(62150,125541,245420,283042,327406),chr=c(1,2,4,5,5))
+qtls<-data.frame(pos=c(23104253,13746533,15359142,10500440,25490969),chr=c(1,2,4,5,5))
+pdata<-cbind(data[,1:2],p,data[,3]-data[,7])
 
-for (chr in 1:5)
+winSize<-500000
+winStep<-50000
+minMarkersInWindow<-20
 
-for (chr in 2:5){
+for (chr in 1:5){
+ x<-which(data[,1]==chr)
+ shifts<-seq(0,winSize-1,winStep)
+
+ unsorted<-sapply(shifts,function(shift){
+  markerCount<-table(floor((pdata[x,2]+shift)/winSize))
+  markerCount<-cbind(as.numeric(rownames(markerCount)),markerCount)
+
+  curData<-pdata[x,][pdata[x,3]<0.2,]
+  windows<-floor((curData[,2]+shift) /winSize)
+  passedCount<- table(windows)
+  windowsToUse<-as.numeric(rownames(passedCount)[passedCount>minMarkersInWindow])
+  markersToUse<- windows %in% windowsToUse
+
+  curData<-curData[markersToUse,]
+  windows<-windows[markersToUse]
+
+  aa<-tapply(curData[,4],windows,function(window){sum(window)})/markerCount[markerCount[,1] %in% unique(windows),2]
+#  tp<-tapply(curData[,3],windows,function(window){(sum(window<0.2)^2)/(length(pdata[x,3])*sum(window[window<0.2]))})
+  tp<-tapply(curData[,3],windows,function(window){(sum(window<0.2)^2)/(sum(window[window<0.2]))})/markerCount[markerCount[,1] %in% unique(windows),2]
+#  pos<-(unique(windows)*winSize)+(winSize+shift)/2
+  pos<-tapply(curData[,2],windows,mean)
+  cbind(pos,tp,aa)
+ },simplify=F)
+ pos<-c(sapply(unsorted,function(i) i[,1]),recursive=T)
+ tp<-c(sapply(unsorted,function(i) i[,2]),recursive=T)
+ aa<-c(sapply(unsorted,function(i) i[,3]),recursive=T)
+ o<-sort.int(pos,index.return=T)
+ sorted<-t(sapply(o$ix,function(i) c(pos[i],tp[i],aa[i])))
+
+# y.loess<-loess(y~x,span=0.75,data.frame(x=sorted[,1],y=sorted[,2]))
+# y.predict<- predict(y.loess,data.frame(x=sorted[,1]))
+
+
+ print(paste("chr:",chr,"no Smooth"))
+ print(sorted[which.max(sorted[,2]),1])
+# print(paste(sorted[which(diff(sign(diff(y.predict)))==-2)+1,1]))
+
+ png(paste("pointypeak_chr",chr,"_smooth_avgPos_winsize",winSize,"_winstep",winStep,".png",sep=""))
+# plot(sorted[,1],sorted[,2]*sorted[,3],type="l",col="green",xlab="",ylab="",yaxt="n",xaxt="n")
+# mtext("allele score",side=4,line=3)
+# axis(4)
+# par(new=T)
+ plot(sorted[,1],sorted[,2],type="l",main=paste("pointy peak, chr",chr,"(winsize: ",winSize," bp, winstep: ",winStep," bp)"),xlab="pos",ylab="allele score")
+
+# plot(sorted[,1],sorted[,2],type="l",main=paste("pointy peak, chr",chr,"(winsize: ",winSize," bp, winstep: ",winStep," bp)"),xlab="pos",ylab="p score")
+# lines(sorted[,1],y.predict,col="green",type="l")
+
+# y.loess<-loess(y~x,span=0.5,data.frame(x=sorted[,1],y=sorted[,2]))
+# y.predict<- predict(y.loess,data.frame(x=sorted[,1]))
+# lines(sorted[,1],y.predict,col="red",type="l")
+# print(paste("chr:",chr,"span:",0.5))
+# print(paste(sorted[which(diff(sign(diff(y.predict)))==-2)+1,1]))
+
+ y.loess<-loess(y~x,span=0.6,data.frame(x=sorted[,1],y=sorted[,2]))
+ y.predict<- predict(y.loess,data.frame(x=sorted[,1]))
+ lines(sorted[,1],y.predict,col="green",type="l")
+ maxIndex<-which(diff(sign(diff(c(-Inf,y.predict,-Inf))))==-2)
+ minIndex<-which(diff(sign(diff(c(-Inf,-y.predict,-Inf))))==-2)
+ 
+ minShift<-0
+ if(maxIndex[1]<minIndex[1]){
+  #starts with a local maxima
+  minShift<--1
+ }else{
+  #starts with a local minima
+  minShift<-0
+ }
+ for(i in 1:length(maxIndex)){
+  abline(v=sorted[maxIndex[i],1],col="blue")
+  if(y.predict[maxIndex[i]]*100>max(y.predict[maxIndex])){
+   checkLow<-FALSE
+   checkHigh<-FALSE
+   minI<-i+minShift
+   if(minI>0&minI<=length(minIndex)){
+    checkLow<-y.predict[maxIndex[i]]>2*y.predict[minIndex[minI]]
+   }else{
+    checkLow<-TRUE
+   }
+   minI<-minI+1
+   if(minI>0&minI<=length(minIndex)){
+    checkHigh<-y.predict[maxIndex[i]]>2*y.predict[minIndex[minI]]
+   }else{
+    checkHigh<-TRUE
+   }
+   if(checkLow && checkHigh){
+    points(sorted[maxIndex[i],1],y.predict[maxIndex[i]],col="blue",pch=8,lwd=3,cex=2)
+   }
+  }
+ }
+
+ print(paste("chr:",chr,"span:",0.6))
+ print(paste(sorted[which(diff(sign(diff(y.predict)))==-2)+1,1]))
+
+# y.loess<-loess(y~x,span=0.9,data.frame(x=sorted[,1],y=sorted[,2]))
+# y.predict<- predict(y.loess,data.frame(x=sorted[,1]))
+# lines(sorted[,1],y.predict,col="blue",type="l")
+# print(paste("chr:",chr,"span:",0.9))
+# print(paste(sorted[which(diff(sign(diff(y.predict)))==-2)+1,1]))
+
+
+
+ for(pos in qtls[qtls$chr==chr,1]){
+  abline(v=pos,col="red")
+ }
+# par(new=T)
+
+ dev.off()
+
+}
+
+for (chr in 1:5){
 png(paste("summary_chr",chr,".png",sep=""))
 x<-which(data[,1]==chr)
-y.loess<-loess(y~x,span=0.75,data.frame(x=data[x,2],y=lp[x]))
+y.loess<-loess(y~x,span=0.75,data.frame(x=data[x,2],y=p[x]))
 y.predict<-predict(y.loess,data.frame(x=data[x,2]))
 
 
@@ -25,10 +146,11 @@ y.predict<-predict(y.loess,data.frame(x=data[x,2]))
 #mtext("frequency",side=4,line=3)
 #lines(data[x,2],data[x,7],col="black",type="l")
 #par(new=T)
-plot(data[x,2],lp[x],xlab="pos",ylab="log(p)",main=paste("chr",chr))
-lines(data[x,2],y.predict,col="red",type="l")
-for(index in qtls[qtls$chr==chr,1]){
- abline(v=data[index,2],col="green")
+#plot(data[x,2],p[x],xlab="pos",ylab="log(p)",main=paste("chr",chr))
+plot(data[x,2],y.predict,col="red",type="l",xlab="pos",ylab="p smoothed",main=paste("chr",chr))
+#lines(data[x,2],y.predict,col="red",type="l")
+for(pos in qtls[qtls$chr==chr,1]){
+ abline(v=pos,col="green")
 }
 dev.off()
 
@@ -47,7 +169,7 @@ reduceDataForChr<-function(data,winSize,chr){
 }
 
 
-mult_one_fun<-function(locus,r,limit=0.9,cols=c(4,5),memmory=1){
+mult_two_fun <- function(locus,r,limit=0.9,memmory=3){
  if((locus%%1000)==0){
   print(locus)
  }
@@ -57,13 +179,55 @@ mult_one_fun<-function(locus,r,limit=0.9,cols=c(4,5),memmory=1){
  assign("shoremap_toUse",shoremap_q>limit,".GlobalEnv")
 # assign("shoremap_toUse",ppois(dist,r,lower.tail=F)>0,".GlobalEnv")
  if(sum(shoremap_toUse)>1){
-  mle2(ll_one_fun,optimizer="optimize",start=list(f=0.5),fixed=list(locus=locus,r=r),lower=0,upper=1)@coef
+  mle2(ll_two_fun,optimizer="optimize",start=list(f=0.5),fixed=list(locus=locus,r=r,memmory=memmory),lower=0,upper=1)@coef
+ }else{
+  sum(shoremap_qtlData[locus,c(4,8)])/sum(shoremap_qtlData[locus,c(4,5,8,9)])
+ }
+
+}
+
+mult_one_fun<-function(locus,r,limit=0.9,memmory=1){
+ if((locus%%1000)==0){
+  print(locus)
+ }
+ cols=c(4,5)
+ if(memmory==2){
+  cols=c(8,9)
+ }
+ dist<-abs(shoremap_qtlData[,2]-shoremap_qtlData[locus,2])
+ assign("shoremap_q",sapply(dist,q_fun,r),".GlobalEnv")
+# assign("shoremap_mod",ppois(dist,r,lower.tail=F),".GlobalEnv")
+ assign("shoremap_toUse",shoremap_q>limit,".GlobalEnv")
+# assign("shoremap_toUse",ppois(dist,r,lower.tail=F)>0,".GlobalEnv")
+ if(sum(shoremap_toUse)>1){
+  mle2(ll_one_fun,optimizer="optimize",start=list(f=0.5),fixed=list(locus=locus,r=r,memmory=memmory),lower=0,upper=1)@coef
  }else{
   shoremap_qtlData[locus,cols[1]]/sum(shoremap_qtlData[locus,cols])
  }
 }
 
-ll_one_fun<-function(f,locus,r=50000,cols=c(4,5),memmory=1){
+ll_two_fun<-function(f,locus,r=50000,memmory=3){
+ cols1<-c(4,5)
+ cols2<-c(8,9)
+ nrOfPlants<-500
+ fc<-floor(f*nrOfPlants)/nrOfPlants
+ if(fc<0 || fc>1){
+  110000+abs(fc-0.5)
+ }else if(sum(shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==locus &shoremap_qtlmem[,3]==memmory )==1) {
+  shoremap_qtlmem[shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==locus &shoremap_qtlmem[,3]==memmory,4]
+ }else{
+  l<- ll_one_fun(fc,locus,r,1)+ll_one_fun(fc,locus,r,2)
+#  print(paste(fc,l,sep=" ## "))
+  assign("shoremap_qtlmem",rbind(shoremap_qtlmem,c(fc,locus,memmory,l)),".GlobalEnv")
+  l
+ }
+}
+
+ll_one_fun<-function(f,locus,r=50000,memmory=1){
+ cols<-c(4,5)
+ if(memmory==2){
+  cols<-c(8,9)
+ }
 # print(summary(q))
  nrOfPlants<-500
  fc<-floor(f*nrOfPlants)/nrOfPlants
