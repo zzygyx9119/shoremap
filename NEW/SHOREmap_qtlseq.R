e1935338e224768641d2d1353ce6b9e3d3f6a22c
karl.nordstrom <karl.nordstrom@gmail.com>
2011-08-19 09:19:13 +0000

    minor improvements and some code for testing

--- NEW/SHOREmap_qtlseq.R
+++ NEW/SHOREmap_qtlseq.R
@@ -22,7 +22,7 @@ if(length(args)==4){
 #Parameters
 winSize<-500000
 winStep<-50000
-minMarkersInWindow<-20
+minMarkersInWindow<-10
 p.valueCutoff<-0.2
 minCoverage <- 4
 maxCoverage <- 300
@@ -31,6 +31,16 @@ maxCoverage <- 300
 hs<-read.table(high_file)
 ls<-read.table(low_file)
 
+#extract instersection of markers
+chrmod<-10**ceiling(log10(max(hs[,2],ls[,2])))
+hs.mod<-hs[,1]*chrmod+hs[,2]
+ls.mod<-ls[,1]*chrmod+ls[,2]
+
+intersect.mod<-intersect(hs.mod,ls.mod)
+hs<-hs[hs.mod %in% intersect.mod,]
+ls<-ls[ls.mod %in% intersect.mod,]
+
+
 hs.cov<-rowSums(hs[,3:5])
 ls.cov<-rowSums(ls[,3:5])
 
@@ -38,9 +48,9 @@ goodCov<-(hs.cov>minCoverage & hs.cov<maxCoverage)&(ls.cov>minCoverage &ls.cov<m
 
 hs<-hs[goodCov,]
 ls<-ls[goodCov,]
-qtls<-data.frame(pos=0,chr=0)
+qtls<-data.frame(pos=-1,chr=-1)
 if(qtl_file==""){
- qtls<-data.frame(pos=0,chr=0)
+ qtls<-data.frame(pos=-1,chr=-1)
 }else{
  qtls<-read.table(qtl_file)
  qtls<-data.frame(pos=qtls[,4],chr=qtls[,3])
@@ -48,10 +58,8 @@ if(qtl_file==""){
 
 
 ##merge
-hs.freq<- rep(1,length(hs[,1]))
-ls.freq<- rep(1,length(ls[,1]))
-#hs.freq<- apply(hs,1,function(x) x[3]/sum(x[3:5])) #needed for calculation of allele score
-#ls.freq<- apply(ls,1,function(x) x[3]/sum(x[3:5])) #needed for calculation of allele
+hs.freq<-hs[,3]/rowSums(hs[,3:5])
+ls.freq<-ls[,3]/rowSums(ls[,3:5])
 
 data<- cbind(hs[,1:2],hs.freq,hs[,3:5],ls.freq,ls[,3:5])
 
@@ -63,7 +71,7 @@ pdata<-cbind(data[,1:2],p,data[,3]-data[,7])
 
 estimates<-c()
 
-for (chr in 1:5){
+for (chr in unique(data[,1])){
  x<-which(data[,1]==chr)
  shifts<-seq(0,winSize-1,winStep)
 
@@ -97,63 +105,69 @@ for (chr in 1:5){
  o<-sort.int(pos,index.return=T)
 # sorted<-t(sapply(o$ix,function(i) c(pos[i],tp[i],aa[i]))) #needed for calculation of allele
  sorted<-t(sapply(o$ix,function(i) c(pos[i],tp[i])))
+ if(dim(sorted)[2]>0){
+  #second smoothing
+  pscore.loess<-loess(pscore~x,span=0.6,data.frame(x=sorted[,1],pscore=sorted[,2]))
+  pscore.predict<- predict(pscore.loess,data.frame(x=sorted[,1]))
 
- #second smoothing
- pscore.loess<-loess(pscore~x,span=0.6,data.frame(x=sorted[,1],pscore=sorted[,2]))
- pscore.predict<- predict(pscore.loess,data.frame(x=sorted[,1]))
+  #plot and print
+  png(paste(outprefix,"_chr",chr,"_winsize",winSize,"_winstep",winStep,".png",sep=""))
 
- #plot and print
- png(paste(outprefix,"_chr",chr,"_winsize",winSize,"_winstep",winStep,".png",sep=""))
-
- plot(sorted[,1],sorted[,2],type="l",main=paste("pointy peak, chr",chr,"(winsize: ",winSize," bp, winstep: ",winStep," bp)"),xlab="pos",ylab="p score")
+  plot(sorted[,1],sorted[,2],type="l",main=paste("pointy peak, chr",chr,"(winsize: ",winSize," bp, winstep: ",winStep," bp)"),xlab="pos",ylab="p score")
 
 
- lines(sorted[,1],pscore.predict,col="green",type="l")
+  lines(sorted[,1],pscore.predict,col="green",type="l")
 
- #identify local min and max values
- maxIndex<-which(diff(sign(diff(c(-Inf,pscore.predict,-Inf))))==-2)
- minIndex<-which(diff(sign(diff(c(-Inf,-pscore.predict,-Inf))))==-2)
- 
- #filter peaks, a good peak must be twice as large as the flanking minimas and at least 1% of the total maxima on the chromosome
- minShift<-0
- if(maxIndex[1]<minIndex[1]){
-  #starts with a local maxima
-  minShift<--1
- }else{
-  #starts with a local minima
-  minShift<-0
- }
- for(i in 1:length(maxIndex)){
-  abline(v=sorted[maxIndex[i],1],col="blue")
-  if(pscore.predict[maxIndex[i]]*100>max(pscore.predict[maxIndex])){
-   checkLow<-FALSE
-   checkHigh<-FALSE
-   minI<-i+minShift
-   if(minI>0&minI<=length(minIndex)){
-    checkLow<-pscore.predict[maxIndex[i]]>2*pscore.predict[minIndex[minI]]
+  #identify local min and max values
+  maxDiff<-sign(diff(c(-Inf,pscore.predict,-Inf)))
+  maxID<-which(maxDiff!=0)
+  maxIndex<-maxID[which(diff(maxDiff[maxID])==-2)]
+  minDiff<-sign(diff(c(-Inf,-pscore.predict,-Inf)))
+  minID<-which(minDiff!=0)
+  minIndex<-minID[which(diff(minDiff[minID])==-2)]
+
+  if(length(maxIndex)>0&&length(minIndex)>0){
+   #filter peaks, a good peak must be twice as large as the flanking minimas and at least 1% of the total maxima on the chromosome
+   minShift<-0
+   if(maxIndex[1]<minIndex[1]){
+    #starts with a local maxima
+    minShift<--1
    }else{
-    checkLow<-TRUE
+    #starts with a local minima
+    minShift<-0
    }
-   minI<-minI+1
-   if(minI>0&minI<=length(minIndex)){
-    checkHigh<-pscore.predict[maxIndex[i]]>2*pscore.predict[minIndex[minI]]
-   }else{
-    checkHigh<-TRUE
-   }
-   if(checkLow && checkHigh){
-    estimates<-rbind(estimates,c(chr,sorted[maxIndex[i],1]))
-    points(sorted[maxIndex[i],1],pscore.predict[maxIndex[i]],col="blue",pch=8,lwd=3,cex=2)
+   for(i in 1:length(maxIndex)){
+    abline(v=sorted[maxIndex[i],1],col="blue")
+    if(pscore.predict[maxIndex[i]]*100>max(pscore.predict[maxIndex])){
+     checkLow<-FALSE
+     checkHigh<-FALSE
+     minI<-i+minShift
+     if(minI>0&minI<=length(minIndex)){
+      checkLow<-pscore.predict[maxIndex[i]]>2*pscore.predict[minIndex[minI]]
+     }else{
+      checkLow<-TRUE
+     }
+     minI<-minI+1
+     if(minI>0&minI<=length(minIndex)){
+      checkHigh<-pscore.predict[maxIndex[i]]>2*pscore.predict[minIndex[minI]]
+     }else{
+      checkHigh<-TRUE
+     }
+     if(checkLow && checkHigh){
+      estimates<-rbind(estimates,c(chr,sorted[maxIndex[i],1]))
+      points(sorted[maxIndex[i],1],pscore.predict[maxIndex[i]],col="blue",pch=8,lwd=3,cex=2)
+     }
+    }
    }
   }
- }
+  
+  #mark the true QTLs
+  for(pos in qtls[qtls$chr==chr,1]){
+   abline(v=pos,col="red")
+  }
  
- #mark the true QTLs
- for(pos in qtls[qtls$chr==chr,1]){
-  abline(v=pos,col="red")
+  dev.off()
  }
-
- dev.off()
-
 }
 
 #print estimates
