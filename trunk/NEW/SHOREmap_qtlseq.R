ba876ef502fece248dc1a5df3f427259f0b97b03
karl.nordstrom@gmail.com <karl.nordstrom@gmail.com@16d75961-40e5-f889-0403-368a60771a35>
2011-09-29 12:50:12 +0000

    peak finding and interval

--- trunk/NEW/SHOREmap_qtlseq.R
+++ trunk/NEW/SHOREmap_qtlseq.R
@@ -1,4 +1,210 @@
+library(bbmle)
 
+mult_two_fun <- function(locus,r,limit=0.9,memmory=3){
+ if((locus%%1000)==0){
+  print(locus)
+ }
+ dist<-abs(shoremap_qtlData[,2]-shoremap_qtlData[locus,2])
+ assign("shoremap_q",sapply(dist,q_fun,r),".GlobalEnv")
+# assign("shoremap_mod",ppois(dist,r,lower.tail=F),".GlobalEnv")
+ assign("shoremap_toUse",shoremap_q>limit,".GlobalEnv")
+# assign("shoremap_toUse",ppois(dist,r,lower.tail=F)>0,".GlobalEnv")
+ if(sum(shoremap_toUse)>1){
+  mle2(ll_two_fun,optimizer="optimize",start=list(f=0.5),fixed=list(locus=locus,r=r,memmory=memmory),lower=0,upper=1)@coef
+ }else{
+  sum(shoremap_qtlData[locus,c(4,8)])/sum(shoremap_qtlData[locus,c(4,5,8,9)])
+ }
+
+}
+
+mult_one_fun<-function(locus,r,limit=0.9,memmory=1){
+ if((locus%%1000)==0){
+  print(locus)
+ }
+ cols=c(4,5)
+ if(memmory==2){
+  cols=c(8,9)
+ }
+ dist<-abs(shoremap_qtlData[,2]-shoremap_qtlData[locus,2])
+ assign("shoremap_q",sapply(dist,q_fun,r),".GlobalEnv")
+# assign("shoremap_mod",ppois(dist,r,lower.tail=F),".GlobalEnv")
+ assign("shoremap_toUse",shoremap_q>limit,".GlobalEnv")
+# assign("shoremap_toUse",ppois(dist,r,lower.tail=F)>0,".GlobalEnv")
+ if(sum(shoremap_toUse)>1){
+  mle2(ll_one_fun,optimizer="optimize",start=list(f=0.5),fixed=list(locus=locus,r=r,memmory=memmory),lower=0,upper=1)@coef
+ }else{
+  shoremap_qtlData[locus,cols[1]]/sum(shoremap_qtlData[locus,cols])
+ }
+}
+
+maxConf<-function(x,minDiff,level,validMin=1,validMax=Inf,maxStart=Inf,minStop=-Inf){
+# print(paste(minDiff,level,sep=" ## "))
+ start=floor(x[1])
+ stop=floor(x[2])
+ validMin<-max(1,validMin)
+ validMax<-min(length(shoremap_qtlData[,1]),validMax)
+# print(paste(start,stop,sep=" ## "))
+ if(start>stop){
+  1000000000+start-stop
+ }else if(start<validMin){
+  1100000000+validMin-start
+ }else if(stop>validMax){
+  1200000000+stop-validMax
+ }else if(start>maxStart){
+  1300000000+start-maxStart
+ }else if(stop<minStop){
+  1400000000+minStop-stop
+ }else if(stop-start<3){
+  1500000000+start-stop
+ }else{
+  interval<-start:stop
+  f1<-estimateFreq_one(interval,1)
+  f2<-estimateFreq_one(interval,2)
+  diff<-abs(f1-f2)
+  if(diff>=minDiff){
+   -(shoremap_qtlData[stop,2]-shoremap_qtlData[start,2])
+  }else{
+   assign("shoremap_interval",interval,".GlobalEnv")
+   f1e<-f1
+   f2e<-f2
+   if(f1e<f2e){
+    f1e<-max(0,f1e-(minDiff-diff)/2)
+    f2e<-f1e+minDiff
+   }else{
+    f1e<-min(f1e+(minDiff-diff)/2,1)
+    f2e<-f1e-minDiff
+   }
+   restricted<-mle2(ll_rest_two,start=list(f1=f1e,f2=f2e),fixed=list(minDiff=minDiff))
+   full<-ll_one_fun(f1,interval,1)+ll_one_fun(f2,interval,2)
+   p<-pchisq(-2*(full-restricted@min),1)
+#   print(paste(p,start,stop,sep=" ## "))
+   if(p<=level){
+    ll<--(shoremap_qtlData[stop,2]-shoremap_qtlData[start,2]+log(1-p))
+    print(paste(p,start,stop,shoremap_qtlData[start,2],shoremap_qtlData[stop,2],ll,sep=" ## "))
+    ll
+   }else{
+    shoremap_qtlData[stop,2]-shoremap_qtlData[start,2]
+   }
+  }
+ }
+}
+
+ll_rest_two<-function(f1,f2,minDiff){
+ if(f1<0||f1>1){
+  110000+abs(f1-0.5)
+ }else if(f2<0 || f2>1){
+  120000+abs(f2-0.5)
+ }else if(abs(f1-f2)<minDiff){
+  130000+abs(f1-f2)
+ }else{
+  ll_one_fun(f1,shoremap_interval,1)+ll_one_fun(f2,shoremap_interval,2)
+ }
+}
+
+estimateFreq_one<-function(interval,memory=1){
+ cols<-c(4,5)
+ if(memory==2){
+  cols<-c(8,9)
+ }
+ f<-colSums(shoremap_qtlData[interval,cols])
+ f[1]/sum(f)
+}
+
+estimateFreq_two<-function(interval){
+ cols<-c(4,5,8,9)
+ f<-colSums(shoremap_qtlData[interval,cols])
+ (f[1]+f[3])/sum(f)
+}
+
+
+ll_one_fun<-function(f,interval,memory=1){
+ cols<-c(4,5)
+ if(memory==2){
+  cols<-c(8,9)
+ }
+ nrOfPlants<-500
+ fc<-floor(f*nrOfPlants)/nrOfPlants
+ if(fc<0 || fc>1){
+  110000+abs(fc-0.5)
+ }else if(sum(shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==min(interval) & shoremap_qtlmem[,3]==max(interval) & shoremap_qtlmem[,4]==memory )==1) {
+  shoremap_qtlmem[shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==min(interval) & shoremap_qtlmem[,3]==max(interval)& shoremap_qtlmem[,4]==memory,5]
+ }else{
+  l<--sum(apply(shoremap_qtlData[interval,cols],1,function(x){dbinom(x[1],size=sum(x),prob=fc,log=TRUE)}))
+  assign("shoremap_qtlmem",rbind(shoremap_qtlmem,c(fc,min(interval),max(interval),memory,l)),".GlobalEnv")
+  l
+ }
+}
+
+ll_two_fun<-function(f,interval,memory=3){
+ cols1<-c(4,5)
+ cols2<-c(8,9)
+ nrOfPlants<-500
+ fc<-floor(f*nrOfPlants)/nrOfPlants
+ if(fc<0 || fc>1){
+  110000+abs(fc-0.5)
+ }else{
+  l<- ll_one_fun(fc,interval,1)+ll_one_fun(fc,interval,2)
+  l
+ }
+}
+
+ll_two_fun_withQ<-function(f,locus,r=50000,memory=3){
+ cols1<-c(4,5)
+ cols2<-c(8,9)
+ nrOfPlants<-500
+ fc<-floor(f*nrOfPlants)/nrOfPlants
+ if(fc<0 || fc>1){
+  110000+abs(fc-0.5)
+ }else if(sum(shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==locus &shoremap_qtlmem[,3]==memory )==1) {
+  shoremap_qtlmem[shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==locus &shoremap_qtlmem[,3]==memory,4]
+ }else{
+  l<- ll_one_fun_withQ(fc,locus,r,1)+ll_one_fun_withQ(fc,locus,r,2)
+#  print(paste(fc,l,sep=" ## "))
+  assign("shoremap_qtlmem",rbind(shoremap_qtlmem,c(fc,locus,memmory,l)),".GlobalEnv")
+  l
+ }
+}
+
+ll_one_fun_withQ<-function(f,locus,r=50000,memory=1){
+ cols<-c(4,5)
+ if(memory==2){
+  cols<-c(8,9)
+ }
+# print(summary(q))
+ nrOfPlants<-500
+ fc<-floor(f*nrOfPlants)/nrOfPlants
+ if(fc<0 || fc>1){
+  110000+abs(fc-0.5)
+ }else if(sum(shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==locus &shoremap_qtlmem[,3]==memory )==1) {
+  shoremap_qtlmem[shoremap_qtlmem[,1]==fc &shoremap_qtlmem[,2]==locus &shoremap_qtlmem[,3]==memory,4]
+ }else{
+#  r<- 250000*100/nrOfPlants
+#  q<- sapply(abs(shoremap_qtlData[,2]-shoremap_qtlData[locus,2]),q_fun,r)
+  l<- -sum(apply(cbind(shoremap_q[shoremap_toUse],shoremap_qtlData[shoremap_toUse,cols]),1,function(x) pa_fun(fc,x[1],x[2],x[3])))
+#  print(paste(fc,l,sep=" ## "))
+  assign("shoremap_qtlmem",rbind(shoremap_qtlmem,c(fc,locus,memmory,l)),".GlobalEnv")
+  l
+ }
+}
+
+pa_fun<-function(f,q,p1,p2){
+ #mod: supposed to be the probability that less than
+ res<-0 
+ if(q==0){
+  #0
+  res<-(1-f)^p1*f^p2
+ }else{
+  res<-(q*f^p1*(1-f)^p2+(1-q)*(1-f)^p1*f^p2)
+ }
+ ifelse(res>0,log(res),-61600)
+}
+
+q_fun<-function(d,r){
+ exp(-d/r)
+}
+
+
+#Run.....
 
 args<-commandArgs(trailingOnly=TRUE)
 
@@ -14,16 +220,15 @@ if(length(args)==4){
  outprefix<-args[3]
  qtl_file=""
 }else{
- print("Usage:\nR --args seqfile1 seqfile2 qtlfile outprefix < SHOREmap_qtlseq.R\nor\nR --args seqfile1 seqfile2 qtlfile outprefix < SHOREmap_qtlseq.R")
+ print("Usage:\nR --args seqfile1 seqfile2 outprefix < SHOREmap_qtlseq.R\nor\nR --args seqfile1 seqfile2 qtlfile outprefix < SHOREmap_qtlseq.R")
  q("no",616,FALSE)
 }
 
 
 #Parameters
 winSize<-500000
-winStep<-50000
+winStep<-10000
 minMarkersInWindow<-10
-p.valueCutoff<-0.2
 minCoverage <- 4
 maxCoverage <- 300
 
@@ -61,119 +266,88 @@ if(qtl_file==""){
 hs.freq<-hs[,3]/rowSums(hs[,3:5])
 ls.freq<-ls[,3]/rowSums(ls[,3:5])
 
-data<- cbind(hs[,1:2],hs.freq,hs[,3:5],ls.freq,ls[,3:5])
-
-#fisher test
-p<-apply(data,1,function(x) fisher.test(matrix(as.numeric(x[c(4,5,8,9)]),nrow=2))$p.value)
-
-pdata<-cbind(data[,1:2],p,data[,3]-data[,7])
-
+data<- cbind(hs[,1:2],hs.freq,hs[,3:5],ls.freq,ls[,3:5],abs(hs.freq-ls.freq))
 
 estimates<-c()
 
-for (chr in unique(data[,1])){
- x<-which(data[,1]==chr)
- shifts<-seq(0,winSize-1,winStep)
+nrOfChrs<-length(unique(data[,1]))
 
- #for each shift value, calculate the p score for each window
- unsorted<-sapply(shifts,function(shift){
-  #total count in each window
-  markerCount<-table(floor((pdata[x,2]+shift)/winSize))
-  markerCount<-cbind(as.numeric(rownames(markerCount)),markerCount)
-  #filter on p-value. Only use markers with p-values below the cutoff
-  passedData<-pdata[x,][pdata[x,3]<p.valueCutoff,]
-  #calculate windows
-  windows<-floor((passedData[,2]+shift) /winSize)
-  #only use windows with more than the minimum number of markers
-  passedCount<- table(windows)
-  windowsToUse<-as.numeric(rownames(passedCount)[passedCount>minMarkersInWindow])
-  markersToUse<- windows %in% windowsToUse
-  passedData<-passedData[markersToUse,]
-  windows<-windows[markersToUse]
-  #calculate scores
-#  aa<-tapply(passedData[,4],windows,function(window){sum(window)})/markerCount[markerCount[,1] %in% unique(windows),2] #needed for calculation of allele
-  tp<-tapply(passedData[,3],windows,function(window){(sum(window)^2)/(sum(window))})/markerCount[markerCount[,1] %in% unique(windows),2]
-#  pos<-(unique(windows)*winSize)+(winSize+shift)/2
-  pos<-tapply(passedData[,2],windows,mean)
-#  cbind(pos,tp,aa) #needed for calculation of allele
-  cbind(pos,tp)
- },simplify=F)
- #extract data and sort
- pos<-c(sapply(unsorted,function(i) i[,1]),recursive=T)
- tp<-c(sapply(unsorted,function(i) i[,2]),recursive=T)
-# aa<-c(sapply(unsorted,function(i) i[,3]),recursive=T) #needed for calculation of allele
- o<-sort.int(pos,index.return=T)
-# sorted<-t(sapply(o$ix,function(i) c(pos[i],tp[i],aa[i]))) #needed for calculation of allele
- sorted<-t(sapply(o$ix,function(i) c(pos[i],tp[i])))
- if(dim(sorted)[2]>0){
-  #second smoothing
-  pscore.loess<-loess(pscore~x,span=0.6,data.frame(x=sorted[,1],pscore=sorted[,2]))
-  pscore.predict<- predict(pscore.loess,data.frame(x=sorted[,1]))
+pdf(paste(outprefix,".plots.pdf",sep=""))
+par(mfrow=c(ceiling(nrOfChrs/2),2))
 
-  #plot and print
-  png(paste(outprefix,"_chr",chr,"_winsize",winSize,"_winstep",winStep,".png",sep=""))
+for(chr in unique(data[,1])){
+ data2<-data[data[,1]==chr,]
+# png()
 
-  plot(sorted[,1],sorted[,2],type="l",main=paste("pointy peak, chr",chr,"(winsize: ",winSize," bp, winstep: ",winStep," bp)"),xlab="pos",ylab="p score")
+ #predicting the number of peaks
+ plot(data2[,2],data2[,11],main=paste("chr",chr),xlabel="pos",ylabel="frequncy difference")
+ y.loess<- loess(y~x, span=0.4,data.frame(x=data2[,2],y=data2[,11]))
+ y.predict<-predict(y.loess,data.frame(x=data2[,2]))
+ lines(data2[,2],y.predict,col="skyblue")
+
+ maxPeakDiff<-sign(diff(c(-Inf,y.predict,-Inf)))
+ maxPeakID<-which(maxPeakDiff!=0)
+ maxPeakIndex<-maxPeakID[which(diff(maxPeakDiff[maxPeakID])==-2)]
+ minPeakDiff<-sign(diff(c(-Inf,-y.predict,-Inf)))
+ minPeakID<-which(minPeakDiff!=0)
+ minPeakIndex<-minPeakID[which(diff(minPeakDiff[minPeakID])==-2)]
+
+ for(peak in maxPeakIndex){
+  #extract region
+  assign("shoremap_qtlmem",matrix(c(-1,-1,-1,-1,-1),nrow=1),".GlobalEnv")
+  lowerBoundary<-max(c(minPeakIndex[minPeakIndex<peak],1))
+  upperBoundary<-min(c(minPeakIndex[minPeakIndex>peak],length(data2[,1])))
+  data3<-data2[lowerBoundary:upperBoundary,]
+  shifts<-seq(0,winSize-1,winStep)
+  assign("shoremap_qtlData",data3,".GlobalEnv")
 
 
-  lines(sorted[,1],pscore.predict,col="green",type="l")
+  #pinpointing window (point estimation)
+  md_long<-sapply(shifts,function(shift){
+   windows<-floor((data3[,2]+shift)/winSize)
+   d<-tapply(1:length(data3[,1]),windows,function(interval){
+    abs(estimateFreq_one(interval,1)-estimateFreq_one(interval,2))
+   })
+   interval<-which(windows==unique(windows)[which.max(d)])
+   list(md=d,best=max(d),interval=interval)
+  })
+  md<-c(md_long[1,],recursive=TRUE)
+  mp<-c(sapply(shifts,function(shift){
+   windows<-floor((data3[,2]+shift)/winSize)
+   tapply(data3[,2],windows,mean)
+  }),recursive=TRUE)
+  wins<-t(sapply(sort(mp,index.return=TRUE)$ix,function(i) c(mp[i],md[i])))
+  lines(wins[,1],wins[,2],col="violetred")
 
-  #identify local min and max values
-  maxDiff<-sign(diff(c(-Inf,pscore.predict,-Inf)))
-  maxID<-which(maxDiff!=0)
-  maxIndex<-maxID[which(diff(maxDiff[maxID])==-2)]
-  minDiff<-sign(diff(c(-Inf,-pscore.predict,-Inf)))
-  minID<-which(minDiff!=0)
-  minIndex<-minID[which(diff(minDiff[minID])==-2)]
+  #adjust frequency
+  minDiff<-max(wins[,2])
+  minDiff<-floor(minDiff*1000)/1000
+  interval<-md_long[3,which.max(c(md_long[2,],recursive=TRUE))]$interval
+  roughEst<-mean(c(min(interval),max(interval)))
+  abline(v=roughEst,col="steelblue")  
 
-  if(length(maxIndex)>0&&length(minIndex)>0){
-   #filter peaks, a good peak must be twice as large as the flanking minimas and at least 1% of the total maxima on the chromosome
-   minShift<-0
-   if(maxIndex[1]<minIndex[1]){
-    #starts with a local maxima
-    minShift<--1
-   }else{
-    #starts with a local minima
-    minShift<-0
-   }
-   for(i in 1:length(maxIndex)){
-    abline(v=sorted[maxIndex[i],1],col="blue")
-    if(pscore.predict[maxIndex[i]]*100>max(pscore.predict[maxIndex])){
-     checkLow<-FALSE
-     checkHigh<-FALSE
-     minI<-i+minShift
-     if(minI>0&minI<=length(minIndex)){
-      checkLow<-pscore.predict[maxIndex[i]]>2*pscore.predict[minIndex[minI]]
-     }else{
-      checkLow<-TRUE
-     }
-     minI<-minI+1
-     if(minI>0&minI<=length(minIndex)){
-      checkHigh<-pscore.predict[maxIndex[i]]>2*pscore.predict[minIndex[minI]]
-     }else{
-      checkHigh<-TRUE
-     }
-     if(checkLow && checkHigh){
-      estimates<-rbind(estimates,c(chr,sorted[maxIndex[i],1]))
-      points(sorted[maxIndex[i],1],pscore.predict[maxIndex[i]],col="blue",pch=8,lwd=3,cex=2)
-     }
-    }
-   }
+  #identify window
+  opt<-optim(fn=maxConf,par=c(min(interval),max(interval)),minDiff=minDiff,level=0.99)
+  bestValue<-Inf
+  while(opt$value<bestValue){
+   bestValue<-opt$value
+   opt<-optim(fn=maxConf,par=c(floor(opt$par[1]),floor(opt$par[2])),minDiff=minDiff,level=0.99)
   }
-  
-  #mark the true QTLs
-  for(pos in qtls[qtls$chr==chr,1]){
-   abline(v=pos,col="red")
-  }
+  rect(data3[opt$par[1],2],0,data3[opt$par[2],2],0.04,col="limegreen",border="limegreen")
+  estimates<-rbind(estimates,c(chr,data3[opt$par[1],2],data3[opt$par[2],2],minDiff,roughEst))
+ }
  
-  dev.off()
+ for(pos in qtls[qtls$chr==chr,1]){
+  abline(v=pos,col="red")
  }
 }
 
+dev.off()
+
 #print estimates
 if(length(estimates[1,])>0){
- colnames(estimates)<-c("#chr","position")
- write.table(estimates,file=paste(outprefix,"_qtlEstimates.csv",sep=""),row.names=F,sep="\t",quote=F)
+ colnames(estimates)<-c("#chr","start","stop","freqDiff","roughEst")
+ write.table(estimates,file=paste(outprefix,".qtlEstimates.csv",sep=""),row.names=F,sep="\t",quote=F)
 }
 
 q("no",0,F)
